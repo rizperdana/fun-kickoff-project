c074a28343b87d020975bb7640bc1e8d51eb2129
Rizki Perdana <perdana.rizki16@gmail.com>
2022-01-08 06:55:48 +0700

    Adding logout and ping spec

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const dotenv = require("dotenv");
 const colors = require("colors");
 const path = require("path");
 const http = require("http");
@@ -14,22 +15,23 @@ const userRouter = require("./routes/user");
 
 const { json, urlencoded } = express;
 
+dotenv.config();
 connectDB();
 const app = express();
 const server = http.createServer(app);
 
 const io = socketio(server, {
-  cors: {
-    origin: "*",
-  },
+    cors: {
+        origin: "*",
+    },
 });
 
 io.on("connection", (socket) => {
-  console.log("connected");
+    console.log("connected");
 });
 
 if (process.env.NODE_ENV === "development") {
-  app.use(logger("dev"));
+    app.use(logger("dev"));
 }
 app.use(json());
 app.use(urlencoded({ extended: false }));
@@ -37,23 +39,23 @@ app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  req.io = io;
-  next();
+    req.io = io;
+    next();
 });
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+    app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
-  );
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
+    );
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
+    app.get("/", (req, res) => {
+        res.send("API is running");
+    });
 }
 
 app.use(notFound);
@@ -61,9 +63,9 @@ app.use(errorHandler);
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-  // Close server & exit process
-  server.close(() => process.exit(1));
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
 });
 
 module.exports = { app, server };
